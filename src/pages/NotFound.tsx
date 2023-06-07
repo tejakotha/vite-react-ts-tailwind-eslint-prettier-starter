0a7e6481fb798e7313182240e504d4f650e2459c
tejakotha <teja0122@gmail.com>
2023-06-07 13:56:27 +0530

    Added tailwind prettier plugin

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,10 @@
 function NotFound() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center leading-relaxed gap-8">
-      <h1 className="text-4xl font-bold text-slate-900 text-center">
+    <div className="flex h-screen flex-col items-center justify-center gap-8 leading-relaxed">
+      <h1 className="text-center text-4xl font-bold text-slate-900">
         404 Error: Unicorns Ate the Page!
       </h1>
-      <p className="max-w-2xl text-lg text-center text-slate-600">
+      <p className="max-w-2xl text-center text-lg text-slate-600">
         Oh no! A mischievous herd of unicorns devoured our page. We've sent our
         finest wizards to charm them away. In the meantime, grab some rainbow
         popcorn, relax, and witness the magical showdown. We apologize for the
